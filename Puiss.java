a840bcebc63dc4e097c0cc3add4101987a418b74
Nada-Iftene <32202745+Nada-Iftene@users.noreply.github.com>
2018-07-30 10:53:02 +0200

    Add files via upload

---
+++ Puiss.java
@@ -0,0 +1,19 @@
+
+public class Puiss{
+	static int puis(int a,int b){
+		int p = 1;
+		for (int i =0 ;i<b;i++){
+			 p *=a;
+			
+		}return(p);
+		
+		
+	}
+
+	public static void main(String[] args) {
+		System.out.print(puis(2,3));
+		// TODO Auto-generated method stub
+
+	}
+
+}
